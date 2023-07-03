427aab4dbe2c981bb954338a396136ff6fb9a0a0
Devonzhang <devonzhang1024@gmail.com>
2023-07-03 17:01:26 +0800

    feat: update docker-compose

--- mongo-init.js
+++ mongo-init.js
@@ -1,1 +1,6 @@
-db.createUser({user: 'book-run', pwd: 'book-run-pwd', roles: [{role: 'readWrite', db: 'book-run'}]});
+db.getSiblingDB('admin').auth(
+    'admin',
+    'root'
+);
+db = db.getSiblingDB('book_run');
+db.createUser({user: 'book-run', pwd: 'book-run-pwd', roles: [{role: 'readWrite', db: 'book_run'}]});
