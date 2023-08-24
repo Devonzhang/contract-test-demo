5c6652280aa9e92cf238d4e511e6e71155ef90ce
Devonzhang <devonzhang1024@gmail.com>
2023-08-24 21:51:34 +0800

    Revert "feat: fail the test"
    This reverts commit 7b5b3a2757c9d8220dd9d6fbbe6d6a2551237079.

--- provider/src/main/java/com/demo/provider/dto/BookResponseDTO.java
+++ provider/src/main/java/com/demo/provider/dto/BookResponseDTO.java
@@ -3,11 +3,13 @@ package com.demo.provider.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.UUID;
+
 @Data
 @Builder
 public class BookResponseDTO {
     private String title;
     private Integer releaseYear;
-//    private UUID id;
+    private UUID id;
     private String redundantField = "abcde";
 }
